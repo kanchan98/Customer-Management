9150a9e50f3cea9e08a7ec6a4759983283c1a27f
Kanchan Nanaware <kanchan.nanaware@simplifyhealthcare.com>
2022-03-04 16:28:04 +0530

    Implimented Customer Management Application

---
+++ CustomerManagement/Controllers/AccountController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CustomerManagement.Models;
+using System.Web.Security;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+
+namespace CustomerManagement.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Models.Membership model)
+        {
+            using (var context = new CustomerDBEntities1())
+            {
+                bool isValid = context.Users.Any(e => e.UserName == model.UserName && e.Password == model.Password);
+                if (isValid)
+                {
+                    FormsAuthentication.SetAuthCookie(model.UserName, false);
+                    return RedirectToAction("Index", "Customers");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Invalid Username and Password");
+                    return View();
+                }
+            }
+        }
+
+        // GET: Account
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignUp(User model)
+        {
+            using (var context = new CustomerDBEntities1())
+            {
+                context.Users.Add(model);
+                context.SaveChanges();
+            }
+            return RedirectToAction("Login");
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ CustomerManagement/Controllers/CustomersController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CustomerManagement.Models;
+using System.Globalization;
+using System.Net.Mail;
+using System.Text;
+
+namespace CustomerManagement.Controllers
+{
+   [Authorize]
+    public class CustomersController : Controller
+    {
+        private CustomerDBEntities1 db = new CustomerDBEntities1();
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            return View(db.Customers.ToList());
+        }
+
+        // GET: Customers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // GET: Customers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Address,MobileNo,PurchaseDate,PurchaseItems,BirthDate,EmailId")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Customers.Add(customer);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(customer);
+        }
+
+        // GET: Customers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: Customers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Address,MobileNo,PurchaseDate,PurchaseItems,BirthDate,EmailId")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(customer).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(customer);
+        }
+
+        // GET: Customers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: Customers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Customer customer = db.Customers.Find(id);
+            db.Customers.Remove(customer);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        
+        public ActionResult EmailToCustomer(int? id)
+        {
+            bool result = false;
+            Customer customer = db.Customers.Find(id);
+            result = SendEmail(customer.EmailId, "Welcome to CMA", "<p> Hello Customer,<br /><br /> Thanks for purchasing. <br /><br /> Regards,<br />CMA.</p>");
+            return RedirectToAction("Index");
+        }
+
+        public bool SendEmail(String toEmail, String subject, string emailBody)
+        {
+            try
+            {
+                string senderEmail = System.Configuration.ConfigurationManager.AppSettings["SenderEmail"].ToString();
+                string senderPassword = System.Configuration.ConfigurationManager.AppSettings["SenderPassword"].ToString();
+
+                SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+                client.EnableSsl = true;
+                client.Timeout = 10000;
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.UseDefaultCredentials = false;
+                client.Credentials = new NetworkCredential(senderEmail, senderPassword);
+                MailMessage mailMesage = new MailMessage(senderEmail, toEmail, subject, emailBody);
+                mailMesage.IsBodyHtml = true;
+                mailMesage.BodyEncoding = UTF7Encoding.UTF8;
+
+                System.Net.Mail.Attachment attachment;
+                attachment = new System.Net.Mail.Attachment("D:\\ProductAttachment.txt");
+                mailMesage.Attachments.Add(attachment);
+
+                client.Send(mailMesage);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
